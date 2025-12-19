e3ed5ae99febddd09cb35f19c63022dcc75ff7bb
amir94214 <104302598+amir94214@users.noreply.github.com>
2025-12-19 20:29:17 +0330

    Add files via upload

---
+++ typeof1.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const movieTitle = "freinds";
+console.log(movieTitle);
+console.log(typeof movieTitle);
+
+const numberOfSeasons = 21;
+console.log(numberOfSeasons);
+console.log(typeof numberOfSeasons);
+
+const isFavorite = true;
+console.log(isFavorite);
+console.log(typeof isFavorite);
+
+const nextSeasoneRelaeseDate = undefined;
+console.log(nextSeasoneRelaeseDate);
+console.log(typeof nextSeasoneRelaeseDate);
+
+const currentSeasoneRating = null;
+console.log(currentSeasoneRating);
+console.log(typeof currentSeasoneRating);
